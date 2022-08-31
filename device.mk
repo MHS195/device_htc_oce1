d79905996a6b343450f30b047142a24162c8ea12
ivanmeler <i_ivan@windowslive.com>
2022-08-31 17:48:58 +0000

    oce: Add custom audio service
    with reverts of:
    LineageOS/android_hardware_interfaces@3f8f599
    LineageOS/android_hardware_interfaces@d90c474
    
    Change-Id: I414048f54933d3d950de23cf360be362637de38c

--- device.mk
+++ device.mk
@@ -119,7 +119,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
-    android.hardware.audio@2.0-service \
+    android.hardware.audio.service.oce \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.soundtrigger@2.0 \
