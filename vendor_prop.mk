6d51dc4acc0e17cd7005883c6deebfaaac37ad4a
TARKZiM <tom8476oo@gmail.com>
2024-09-08 14:43:31 +0800

    oce: prop: Add bpf hack

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -78,7 +78,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # BPF
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.kernel.ebpf.supported=false
+    ro.kernel.ebpf.supported=false \
+    bpf.progs_loaded=1
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
