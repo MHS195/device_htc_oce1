b7a931b3616289a59efc6c7f703eac9050cc9148
MHS195 <79396663+MHS195@users.noreply.github.com>
2025-12-24 01:54:28 +0300

    Add inheritance from lindroid.mk to device.mk

--- device.mk
+++ device.mk
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
+$(call inherit-product, vendor/lindroid/lindroid.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Get non-open-source specific aspects
