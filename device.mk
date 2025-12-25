6f6a812181cc45e17c4f31ee81ed20a011596d51
MHS195 <79396663+MHS195@users.noreply.github.com>
2025-12-25 15:54:35 +0000

    Update device.mk

--- device.mk
+++ device.mk
@@ -499,7 +499,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service.legacy \
+    android.hardware.wifi@1.0-service \
     ipacm \
     IPACM_cfg.xml \
     libwpa_client \
